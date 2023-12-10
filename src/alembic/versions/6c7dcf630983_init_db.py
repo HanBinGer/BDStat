aec3f421f6a87506f507f2b5771a44c0ac5739cc
HanBinGer <elafom3@yandex.ru>
2023-12-10 22:54:30 +0300

    add migrations with alembic

---
+++ src/alembic/versions/6c7dcf630983_init_db.py
@@ -0,0 +1,26 @@
+"""init db
+
+Revision ID: 6c7dcf630983
+Revises: 
+Create Date: 2023-12-09 22:24:46.858801
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6c7dcf630983'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
